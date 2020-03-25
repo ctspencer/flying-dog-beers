d763293377ae218a36cb51e9b0e081ccb5304db0
ctspencer <62153750+ctspencer@users.noreply.github.com>
2020-03-25 16:28:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_cytoscape as cyto
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -10,20 +11,23 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
-])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+app.layout = html.Div([
+    cyto.Cytoscape(
+        id='cytoscape-nodes',
+        layout={'name': 'preset'},
+        style={'width': '100%', 'height': '1000px'},
+        elements=[
+            {'data': {'id': 'MThub', 'label': 'Major Topics'}, 'position': {'x': 225, 'y': 175}, 'grabbable': False},
+            {'data': {'id': 'MT1', 'label': 'Multidisciplinary Initiatives'}, 'position': {'x': 75, 'y': 75}},
+            {'data': {'id': 'MT2', 'label': 'Vulnerable Populations'}, 'position': {'x': 225, 'y': 275}},
+            {'data': {'id': 'MT3', 'label': 'Education & Awareness'}, 'position': {'x': 375, 'y': 75}},
+            {'data': {'source': 'MThub', 'target': 'MT1'}},
+            {'data': {'source': 'MThub', 'target': 'MT2'}},
+            {'data': {'source': 'MThub', 'target': 'MT3'}},
+        ]
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
