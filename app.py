7880e17f70af44e11bb6c968f2b5080e0eb5fe1f
ctspencer <62153750+ctspencer@users.noreply.github.com>
2020-03-25 15:59:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,17 @@ import dash_cytoscape as cyto
 import plotly.graph_objs as go
 
 app.layout = html.Div([
-    cyto.Cytoscape(
-        id='cytoscape-nodes',
-        layout={'name': 'preset'},
-        style={'width': '100%', 'height': '1000px'},
-        elements=[
-            {'data': {'id': 'MThub', 'label': 'Major Topics'}, 'position': {'x': 225, 'y': 175}, 'grabbable': False},
-            {'data': {'id': 'MT1', 'label': 'Multidisciplinary Initiatives'}, 'position': {'x': 75, 'y': 75}},
-            {'data': {'id': 'MT2', 'label': 'Vulnerable Populations'}, 'position': {'x': 225, 'y': 275}},
-            {'data': {'id': 'MT3', 'label': 'Education & Awareness'}, 'position': {'x': 375, 'y': 75}},
-            {'data': {'source': 'MThub', 'target': 'MT1'}},
-            {'data': {'source': 'MThub', 'target': 'MT2'}},
-            {'data': {'source': 'MThub', 'target': 'MT3'}},
-        ]
-    )
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
 ])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
 
 
 if __name__ == '__main__':
